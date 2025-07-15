a912ef5e6c7c10263cc7135500ca0c819b3549f2
Jvitor6 <j.alves@gmail.com>
2025-07-15 17:14:13 -0300

    voltei com o separador de titulos

--- src/main.py
+++ src/main.py
@@ -2,13 +2,20 @@ from docx import Document
 from docx.shared import Pt, Cm
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
+def separador_titulos(documento):
+    secoes = []
+    for paragrafo in documento.paragraphs:
+        estilo = paragrafo.style.name
+        texto = paragrafo.text.strip()
+        if estilo.startswith("Heading") and texto:
+            secoes.append(texto)
+    return secoes
 
-def aplicar_fonte_arial(run):
+def aplicar_fonte(run):
     run.font.name = 'Arial'
     run.font.size = Pt(12)
 
-
-def formatador_abnt(documento):
+def formatador_abnt(documento,nome):
     for section in documento.sections:
         section.top_margin = Cm(3)
         section.bottom_margin = Cm(2)
@@ -26,7 +33,7 @@ def formatador_abnt(documento):
             paragrafo.paragraph_format.first_line_indent = Pt(0)
             for run in paragrafo.runs:
                 run.bold = True
-                aplicar_fonte_arial(run)
+                aplicar_fonte(run)
                 run.font.size = Pt(14)
 
         elif estilo == "Heading 2":
@@ -37,7 +44,7 @@ def formatador_abnt(documento):
             for run in paragrafo.runs:
                 run.bold = True
                 run.italic = True
-                aplicar_fonte_arial(run)
+                aplicar_fonte(run)
 
         elif 'REFERÊNCIAS' in texto_original.upper():
             paragrafo.alignment = WD_ALIGN_PARAGRAPH.LEFT
@@ -48,7 +55,7 @@ def formatador_abnt(documento):
             for run in paragrafo.runs:
                 run.bold = False
                 run.italic = False
-                aplicar_fonte_arial(run)
+                aplicar_fonte(run)
 
         else:
             paragrafo.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
@@ -58,10 +65,13 @@ def formatador_abnt(documento):
             for run in paragrafo.runs:
                 run.bold = False
                 run.italic = False
-                aplicar_fonte_arial(run)
-        print(paragrafo.text)
-    documento.save("teste_formatado.docx")
+                aplicar_fonte(run)
+        #print(paragrafo.text)
+    documento.save(nome)
+    print(f"\ndocumento formatado salvo como {nome}")
 
 doc = "teste.docx"
 documento = Document(doc)
-formatador_abnt(documento)
+sec = separador_titulos(documento)
+print(f"\nSecoes: {sec}")
+formatador_abnt(documento,"teste_formatado.docx")
